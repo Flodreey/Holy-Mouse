c09eff6b536db8ed37a35af464387285cd04196e
André Florentin <andre_florentin@t-online.de>
2023-02-19 16:54:47 +0100

    changed QuestMessageScript so that you can give the displayed text as parameter to the ShowMessage Method

--- Assets/Scripts/QuestMessageScript.cs
+++ Assets/Scripts/QuestMessageScript.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EndQuestMessageScript : MonoBehaviour
 {
-    [SerializeField] GameObject messageDisplay;
+    [SerializeField] GameObject display;
+    [SerializeField] TextMeshProUGUI textField;
     [SerializeField] int secondsToShowMessage = 5;
+
+    string defaultText = "Super! Du hast die Quest erfolgreich gemeistert. Kehre jetzt zu deinem Versteck zurueck, um in die naechste Quest zu gelangen.";
 
 
     // Start is called before the first frame update
     void Start()
     {
-        messageDisplay.SetActive(false);
+        display.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,15 +27,21 @@ public class EndQuestMessageScript : MonoBehaviour
 
     public void ShowMessage()
     {
+        ShowMessage(defaultText);
+    }
+
+    public void ShowMessage(string text)
+    {
+        textField.text = text;
         StartCoroutine(ShowMessageForSeconds(secondsToShowMessage));
     }
 
     IEnumerator ShowMessageForSeconds(int seconds)
     {
-        messageDisplay.SetActive(true);
+        display.SetActive(true);
 
         yield return new WaitForSeconds(seconds);
 
-        messageDisplay.SetActive(false);
+        display.SetActive(false);
     }
 }
